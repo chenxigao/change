156d4490904b9c3d201e1a9aee4ced930dfeb58f
vagrant <vagrant@homestead>
2018-12-13 09:08:29 +0000

    Finish user auth

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -2,6 +2,14 @@
 @section('title','欢乐家园')
 
 @section('content')
-    <h1>欢迎来到欢乐家园</h1>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
+    <h1 align="center">欢迎来到欢乐家园</h1>
 
 @stop
