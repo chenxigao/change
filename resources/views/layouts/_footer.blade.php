741a4442944e03e9a44e848c562c279d89fcccb3
vagrant <vagrant@homestead>
2018-12-13 02:18:39 +0000

    Finish page layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/u/6854613645/home?topnav=1&wvr=6" target="_blank">Springlight</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title','欢乐家园')
+
+@section('content')
+    <h1>欢迎来到欢乐家园</h1>
+
+@stop
