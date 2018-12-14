d006e0aef7d2d6ddd47dccf1515605defa0efb0d
vagrant <vagrant@homestead>
2018-12-14 10:17:16 +0000

    Move User model to folder

--- resources/views/emails/reset_password.blade.php
+++ resources/views/emails/reset_password.blade.php
@@ -1,14 +1,18 @@
-@component('mail::message')
-    #重置密码
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>重置密码</title>
+</head>
+<body>
 
-    你好，{{ $username }}！ 点击下面的链接，重置密码。
+<p>你好，{{ $username }}！点击下面的链接，重置密码：<a href="{{ url(config('app.url').route('password.reset', $token, false)) }}">
+        {{  url(config('app.url').route('password.reset', $token, false)) }}</a></p>
 
-    @component('mail::button',['url' => url(config('app.url').route('password.reset', $token, false))])
-        重置密码
-    @endcomponent
+如果非本人操作，请忽略这封邮件。
 
-    如非本人操作，请忽略这个邮件！
-    你的，<br/>>
-    {{ config('app.name') }}
+<p>你的，<br> {{ config('app.name') }}</p>
 
-@endcomponent
+</body>
+
+</html>
